81211b786f60299804abf05ecdccb8ac4377dc98
reetkaurbhamra <reet.rkb@gmail.com>
2024-11-07 14:12:33 -0500

    adding models and views and controllers

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+if  (process.env.NODE_ENV !== 'production' ){
+  require('dotenv').config();
+}
+
+
 const express = require('express');
 const app = express();
+const espressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index');
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(espressLayouts);
+app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-    console.log('Here');
-    res.render("index", {text: "1234"});
-});
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
-app.listen(3000);
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3000);
