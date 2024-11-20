309165a16825be6235193da2adffd140c7a3f775
reetkaurbhamra <reet.rkb@gmail.com>
2024-11-20 17:34:07 -0500

    joinging fleet schema with PurchaseHistory scchema

--- models/buyHistory.js
+++ models/buyHistory.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const buyHistorySchema = new mongoose.Schema({
     purchaseId: { type: Number, required: true },
     userId: { type: String, ref: 'User', required: true },
-    productId: { type: Number, ref: 'Product', required: true },
+    plateNumber: { type: Number, ref: 'Fleet', required: true },
     quantity: { type: Number, required: true },
     purchaseDate: { type: Date, required: true },
 });
